6cae9724874533aa471ca54f64461fbe98c4d5ad
kevin regenrek <hello@regenrek.at>
2019-12-21 09:36:20 +0100

    fixing path

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,68 +1,84 @@
+const path = require("path");
+
+/* https://github.com/jake-101/bael-template */
+function getDynamicPaths(urlFilepathTable) {
+  return [].concat(
+    ...Object.keys(urlFilepathTable).map(url => {
+      var filepathGlob = urlFilepathTable[url];
+      return glob
+        .sync(filepathGlob, { cwd: "content" })
+        .map(filepath => `${url}/${path.basename(filepath, ".md")}`);
+    })
+  );
+}
+
 export default {
   /*
-  ** Rendering mode
-  ** Doc: https://nuxtjs.org/api/configuration-mode
-  */
-  mode: 'universal',
+   ** Rendering mode
+   ** Doc: https://nuxtjs.org/api/configuration-mode
+   */
+  mode: "universal",
 
   /*
-  ** Headers of the page
-  ** Doc: https://vue-meta.nuxtjs.org/api/#metainfo-properties
-  */
+   ** Headers of the page
+   ** Doc: https://vue-meta.nuxtjs.org/api/#metainfo-properties
+   */
   head: {
-    title: 'Nuxt.js starter for CSB',
+    title: "Nuxt.js starter for CSB",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Official Nuxt.js starter for CodeSandBox' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: "Official Nuxt.js starter for CodeSandBox"
+      }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
 
   /*
-  ** Global CSS
-  ** Doc: https://nuxtjs.org/api/configuration-css
-  */
+   ** Global CSS
+   ** Doc: https://nuxtjs.org/api/configuration-css
+   */
   css: [],
 
   /*
-  ** Plugins to load before mounting the App
-  ** Doc: https://nuxtjs.org/guide/plugins
-  */
+   ** Plugins to load before mounting the App
+   ** Doc: https://nuxtjs.org/guide/plugins
+   */
   plugins: [],
 
   /*
-  ** Nuxt.js modules
-  ** Doc: https://nuxtjs.org/guide/modules
-  */
+   ** Nuxt.js modules
+   ** Doc: https://nuxtjs.org/guide/modules
+   */
   modules: [
     // Doc: https://http.nuxtjs.org
-    '@nuxt/http',
+    "@nuxt/http",
     // TODO: Remove it if you want to eject from codeSandbox
-    './codesandbox'
+    "./codesandbox"
   ],
 
   /*
-  ** HTTP module configuration
-  */
+   ** HTTP module configuration
+   */
   http: {
     // See https://http.nuxtjs.org/api/#options
   },
 
   /*
-  ** Build configuration
-  ** Doc: https://nuxtjs.org/api/configuration-build
-  */
- build: {
-  extend(config, ctx) {
+   ** Build configuration
+   ** Doc: https://nuxtjs.org/api/configuration-build
+   */
+  build: {
+    extend(config, ctx) {
       // add frontmatter-markdown-loader
-      config.module.rules.push(
-          {
-              test: /\.md$/,
-              include: path.resolve(__dirname, "content")
-              loader: "frontmatter-markdown-loader",
-          }
-      );
+      config.module.rules.push({
+        test: /\.md$/,
+        include: path.resolve(__dirname, "content"),
+        loader: "frontmatter-markdown-loader"
+      });
     }
   }
 };
